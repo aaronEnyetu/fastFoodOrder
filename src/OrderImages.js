018b47fc504a5671de487752a1f892d80ad2e902
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-14 17:58:31 -0700

    image components working

--- src/OrderImages.js
+++ src/OrderImages.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function OrderImages({ drinkId, desertId, pizzaId, saladId }) {
   return (
-    <div className="orderImages">
+    <div className="order">
       <img src={`drink-${drinkId}.jpg`} />
       <img src={`desert-${desertId}.jpg`} />
       <img src={`pizza-${pizzaId}.jpg`} />
